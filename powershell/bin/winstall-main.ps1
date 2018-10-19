947223c613af1fb45279f95ea1e8ba47eedb6d92
Ad3t0 <ad3t000@gmail.com>
2018-10-19 02:41:08 -0600

    updates

---
+++ powershell/bin/winstall-main.ps1
@@ -0,0 +1,262 @@
+###########################################################################
+# 
+# Windows 10 Fresh Install Cleaner
+#
+##########################################################################
+$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+if ($myWindowsPrincipal.IsInRole($adminRole))
+{
+	$Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
+	$Host.UI.RawUI.BackgroundColor = "DarkBlue"
+	clear-host
+}
+else
+{
+	$newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+	$newProcess.Arguments = $myInvocation.MyCommand.Definition;
+	$newProcess.Verb = "runas";
+	[System.Diagnostics.Process]::Start($newProcess);
+	exit
+}
+##############
+
+
+###########################################################################
+# Disable Windows Store automatic install service
+##########################################################################
+
+net stop InstallService
+sc config InstallService start=disabled
+
+###########################################################################
+# Kill explorer.exe
+##########################################################################
+
+Invoke-Expression "taskkill /f /im explorer.exe"
+
+###########################################################################
+# Remove all Windows store apps expect WindowsStore, Calculator and Photos
+##########################################################################
+
+Get-AppxPackage -AllUsers | where-object {$_.name -notlike "*Microsoft.WindowsStore*"} | where-object {$_.name -notlike "*Microsoft.WindowsCalculator*"} | where-object {$_.name -notlike "*Microsoft.Windows.Photos*"} | Remove-AppxPackage
+Get-AppxProvisionedPackage -online | where-object {$_.packagename -notlike "*Microsoft.WindowsStore*"} | where-object {$_.packagename -notlike "*Microsoft.WindowsCalculator*"} | where-object {$_.packagename -notlike "*Microsoft.Windows.Photos*"} |Remove-AppxProvisionedPackage -online
+
+###########################################################################
+# Taskbar pinapp function
+##########################################################################
+
+
+function findTheNeedle ($needle, $haystack, $startIndexInHaystack=0, $needlePartsThatDontMatter=@())
+{
+    $haystackLastIndex = ($haystack.Length - 1)
+    $needleLastIndex = ($needle.Length - 1)
+    $needleCurrentIndex = 0
+    $haystackCurrentIndex = $startIndexInHaystack
+    while ($haystackCurrentIndex -lt $haystackLastIndex)
+    {
+        if ($haystack[$haystackCurrentIndex] -eq $needle[$needleCurrentIndex]  -or ($needleCurrentIndex -in $needlePartsThatDontMatter))
+        {
+            $startIndex = $haystackCurrentIndex
+            while ($haystack[$haystackCurrentIndex] -eq $needle[$needleCurrentIndex] -or ($needleCurrentIndex -in $needlePartsThatDontMatter))
+            {
+                $needleCurrentIndex += 1
+                $haystackCurrentIndex += 1
+            }
+            if (($needleCurrentIndex - 1) -eq $needleLastIndex)
+            {
+                return ($startIndex + 1)
+            }
+            $needleCurrentIndex = 0
+            $haystackCurrentIndex = $startIndex
+        }
+        $haystackCurrentIndex += 1
+    }
+    return -1
+}
+
+function findTheNeedleReverse ($needle, $haystack, $startIndexInHaystack=0, $needlePartsThatDontMatter=@())
+{
+    $needleLastIndex = ($needle.Length - 1)
+    $needleCurrentIndex = $needleLastIndex
+    $haystackCurrentIndex = $startIndexInHaystack
+    while ($haystackCurrentIndex -gt 0)
+    {
+
+        if ($haystack[$haystackCurrentIndex] -eq $needle[$needleCurrentIndex]  -or ($needleCurrentIndex -in $needlePartsThatDontMatter))
+        {
+            $startIndex = $haystackCurrentIndex
+            while ($haystack[$haystackCurrentIndex] -eq $needle[$needleCurrentIndex] -or ($needleCurrentIndex -in $needlePartsThatDontMatter))
+            {
+                $needleCurrentIndex -= 1
+                $haystackCurrentIndex -= 1
+            }
+            if (($needleCurrentIndex + 1) -eq 0)
+            {
+                return ($haystackCurrentIndex + 1)
+            }
+            $needleCurrentIndex = $needleLastIndex
+            $haystackCurrentIndex = $startIndex
+        }
+        $haystackCurrentIndex -= 1
+    }
+    return -1
+}
+$taskbarEntryHeaderBytes = @(0,51,6,0,0,20,0,31,128,155,212,52);
+$taskbarEntryHeaderBytesThatDontMatter = @(0,1,2,5,9,10);
+$taskbarEntryTrailerBytes = @(34,0,0,0,30,0,239,190,2,0,85,0,115,0,101,0,114,0,80,0,105,0,110,0,110,0,101,0,100,0,0,0,59,5,0,0);
+$taskbarEntryTrailerBytesThatDontMatter = @(32,33);
+$apps = @(
+    @{
+        "appName" = "Microsoft Edge"
+        "appThumbprint" = @(38,0,0,0,77,0,105,0,99,0,114,0,111,0,115,0,111,0,102,0,116,0,46,0,77,0,105,0,99,0,114,0,111,0,115,0,111,0,102,0,116,0,69,0,100,0,103,0,101);
+        "taskbarEntryHeaderBytes" = $taskbarEntryHeaderBytes
+        "taskbarEntryHeaderBytesThatDontMatter" = $taskbarEntryHeaderBytesThatDontMatter
+        "taskbarEntryTrailerBytes" = $taskbarEntryTrailerBytes
+        "taskbarEntryTrailerBytesThatDontMatter" = $taskbarEntryTrailerBytesThatDontMatter
+    }
+    @{
+        "appName" = "Microsoft Windows Store"
+        "appThumbprint" = @(37,0,0,0,77,0,105,0,99,0,114,0,111,0,115,0,111,0,102,0,116,0,46,0,87,0,105,0,110,0,100,0,111,0,119,0,115,0,83,0,116,0,111,0,114,0,101);
+        "taskbarEntryHeaderBytes" = $taskbarEntryHeaderBytes
+        "taskbarEntryHeaderBytesThatDontMatter" = $taskbarEntryHeaderBytesThatDontMatter
+        "taskbarEntryTrailerBytes" = $taskbarEntryTrailerBytes
+        "taskbarEntryTrailerBytesThatDontMatter" = $taskbarEntryTrailerBytesThatDontMatter
+    }
+)
+$atLeastOneAppIsPinned = $false
+foreach ($app in $apps)
+{
+    $haystack = (Get-ItemProperty -Path "hkcu:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Taskband" -Name "Favorites").Favorites
+    $needleStart = findTheNeedle $app["appThumbprint"] $haystack
+    if ($needleStart -ne -1)
+    {
+        $firstIndexAfterNeedle = $needleStart + $app["appThumbprint"].Length
+        $lastIndexOfEntry = (findTheNeedle $app["taskbarEntryTrailerBytes"] $haystack $firstIndexAfterNeedle $app["taskbarEntryTrailerBytesThatDontMatter"]) + ($app["taskbarEntryTrailerBytes"].Length - 1)
+        if (($lastIndexOfEntry) -ne -1)
+        {
+            $firstIndexOfEntry = findTheNeedleReverse $app["taskbarEntryHeaderBytes"] $haystack $needleStart $app["taskbarEntryHeaderBytesThatDontMatter"]
+            if (($firstIndexOfEntry) -ne -1)
+            {
+                $atLeastOneAppIsPinned = $true
+                if ($firstIndexOfEntry -eq 0) 
+                {
+                    $newArray = $haystack[$lastIndexOfEntry..($haystack.Length)]
+                }
+                else
+                {
+                    $newArray = $haystack[0..($firstIndexOfEntry - 1)] + $haystack[$lastIndexOfEntry..($haystack.Length)]
+                }
+                New-ItemProperty -Path "hkcu:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Taskband" -Name "Favorites" -Value $newArray -PropertyType Binary -Force | Out-Null
+            }
+        }
+    }
+}
+
+###########################################################################
+# Pinapp function
+##########################################################################
+
+function Pin-App {    param(
+        [string]$appname,
+        [switch]$unpin
+    )
+    try{
+        if ($unpin.IsPresent){
+            ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $appname}).Verbs() | ?{$_.Name.replace('&','') -match 'Von "Start" lösen|Unpin from Start'} | %{$_.DoIt()}
+            return "App '$appname' unpinned from Start"
+        }else{
+            ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $appname}).Verbs() | ?{$_.Name.replace('&','') -match 'An "Start" anheften|Pin to Start'} | %{$_.DoIt()}
+            return "App '$appname' pinned to Start"
+        }
+    }catch{
+        Write-Error "Error Pinning/Unpinning App! (App-Name correct?)"
+    }
+}
+
+###########################################################################
+# Unpin everything from the start menu
+##########################################################################
+
+Get-StartApps | ForEach-Object { Pin-App $_.name -unpin }
+
+###########################################################################
+# Pin these apps to the start menu
+##########################################################################
+
+Pin-App "Calculator" -pin
+Pin-App "Photos" -pin
+Pin-App "File Explorer" -pin
+Pin-App "Control Panel" -pin
+Pin-App "Notepad" -pin
+Pin-App "Remote Desktop Connection" -pin
+
+###########################################################################
+# Disable Taskview and People icons from the taskbar
+##########################################################################
+
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People" -Name "PeopleBand" -value 0
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton" -value 0
+Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer" -Name "DisableNotificationCenter" -Type DWord -Value 1
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\PushNotifications" -Name "ToastEnabled" -Type DWord -Value 0
+
+###########################################################################
+# Delete all desktop icons
+##########################################################################
+
+Remove-Item C:\Users\*\Desktop\*lnk –Force
+
+###########################################################################
+# Turn Off All Windows 10 Telemetry
+##########################################################################
+
+
+(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/hahndorf/Set-Privacy/master/Set-Privacy.ps1') | out-file .\Set-Privacy.ps1 -force
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+.\Set-Privacy.ps1 -Strong -admin
+
+###########################################################################
+# Remove OneDrive
+##########################################################################
+
+Import-Module -DisableNameChecking $PSScriptRoot\..\lib\force-mkdir.psm1
+Import-Module -DisableNameChecking $PSScriptRoot\..\lib\take-own.psm1
+
+echo "73 OneDrive process and explorer"
+taskkill.exe /F /IM "OneDrive.exe"
+taskkill.exe /F /IM "explorer.exe"
+
+echo "Remove OneDrive"
+if (Test-Path "$env:systemroot\System32\OneDriveSetup.exe") {
+    & "$env:systemroot\System32\OneDriveSetup.exe" /uninstall
+}
+if (Test-Path "$env:systemroot\SysWOW64\OneDriveSetup.exe") {
+    & "$env:systemroot\SysWOW64\OneDriveSetup.exe" /uninstall
+}
+
+echo "Disable OneDrive via Group Policies"
+force-mkdir "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\OneDrive"
+sp "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\OneDrive" "DisableFileSyncNGSC" 1
+
+echo "Removing OneDrive leftovers trash"
+rm -Recurse -Force -ErrorAction SilentlyContinue "$env:localappdata\Microsoft\OneDrive"
+rm -Recurse -Force -ErrorAction SilentlyContinue "$env:programdata\Microsoft OneDrive"
+rm -Recurse -Force -ErrorAction SilentlyContinue "C:\OneDriveTemp"
+
+echo "Remove Onedrive from explorer sidebar"
+New-PSDrive -PSProvider "Registry" -Root "HKEY_CLASSES_ROOT" -Name "HKCR"
+mkdir -Force "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}"
+sp "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" "System.IsPinnedToNameSpaceTree" 0
+mkdir -Force "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}"
+sp "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" "System.IsPinnedToNameSpaceTree" 0
+Remove-PSDrive "HKCR"
+
+echo "Removing run option for new users"
+reg load "hku\Default" "C:\Users\Default\NTUSER.DAT"
+
+###########################################################################
+# Start explorer.exe
+##########################################################################
+
+Invoke-Expression "start explorer.exe"
--- powershell/bin/winstall-unpin.ps1
+++ powershell/bin/winstall-unpin.ps1
@@ -1,173 +1,26 @@
-Get-AppxPackage -AllUsers | where-object {$_.name -notlike "*Microsoft.WindowsCalculator*"} | Remove-AppxPackage
-#Get-AppxProvisionedPackage -online | where-object {$_.packagename -notlike "*Microsoft.WindowsCalculator*"} | Remove-AppxProvisionedPackage -online
-
-function findTheNeedle ($needle, $haystack, $startIndexInHaystack=0, $needlePartsThatDontMatter=@())
+###########################################################################
+# 
+# Windows 10 Unpin app
+#
+##########################################################################
+$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+if ($myWindowsPrincipal.IsInRole($adminRole))
 {
-    # Save the last index for the current haystack array and the needle array
-    $haystackLastIndex = ($haystack.Length - 1)
-    $needleLastIndex = ($needle.Length - 1)
-
-    # Used to keep track of the current location in the byte arrays
-    $needleCurrentIndex = 0
-    $haystackCurrentIndex = $startIndexInHaystack
-
-    # Loop through the current haystack array
-    while ($haystackCurrentIndex -lt $haystackLastIndex)
-    {
-        if ($haystack[$haystackCurrentIndex] -eq $needle[$needleCurrentIndex]  -or ($needleCurrentIndex -in $needlePartsThatDontMatter))
-        {
-            # If the current value of the haystack array matches the first value of the needle array,
-            # we save the current haystack array index.
-            $startIndex = $haystackCurrentIndex
-
-            # We then proceed to compare the next item in the haystack array with the next value in the
-            # needle array until we no longer get a match, like a greedy regex
-            while ($haystack[$haystackCurrentIndex] -eq $needle[$needleCurrentIndex] -or ($needleCurrentIndex -in $needlePartsThatDontMatter))
-            {
-                $needleCurrentIndex += 1
-                $haystackCurrentIndex += 1
-            }
-
-            # If the last match was the last item in the needle array, we have located the entire needle
-            # array inside the haystack array. This means the needle is present. Return the indexes in the haystack
-            # array where the needle array begins.
-            if (($needleCurrentIndex - 1) -eq $needleLastIndex)
-            {
-                return ($startIndex + 1)
-            }
-
-            # Reset the needle index counter
-            $needleCurrentIndex = 0
-
-            # Move the haystack index back to the location of the match that caused us to enter this if block.
-            $haystackCurrentIndex = $startIndex
-        }
-
-        # Step to the next item in the haystack array
-        $haystackCurrentIndex += 1
-    }
-    return -1
+	$Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
+	$Host.UI.RawUI.BackgroundColor = "DarkBlue"
+	clear-host
 }
-
-function findTheNeedleReverse ($needle, $haystack, $startIndexInHaystack=0, $needlePartsThatDontMatter=@())
+else
 {
-    # Save the last index for the current taskbar array and the needle array
-    $needleLastIndex = ($needle.Length - 1)
-
-    # Used to keep track of the current location in the byte arrays
-    $needleCurrentIndex = $needleLastIndex
-    $haystackCurrentIndex = $startIndexInHaystack
-
-    # Loop through the current taskbar array
-    while ($haystackCurrentIndex -gt 0)
-    {
-
-        if ($haystack[$haystackCurrentIndex] -eq $needle[$needleCurrentIndex]  -or ($needleCurrentIndex -in $needlePartsThatDontMatter))
-        {
-            # If the current value of the registry byte array matches the first value of the app byte array,
-            # we save the current registry byte array index.
-            $startIndex = $haystackCurrentIndex
-
-            # We then proceed to compare the next item in the registry byte array with the next value in the
-            # app byte array until we no longer get a match, like a greedy regex.
-            while ($haystack[$haystackCurrentIndex] -eq $needle[$needleCurrentIndex] -or ($needleCurrentIndex -in $needlePartsThatDontMatter))
-            {
-                $needleCurrentIndex -= 1
-                $haystackCurrentIndex -= 1
-            }
-
-            # If the last match was the last item in the app byte array, we have located the entire app byte
-            # array inside the registry byte array. This means Edge is pinned. Save the indexes in the registry
-            # byte array where the Edge byte array begins and ends ($startIndex and $endIndex).
-            if (($needleCurrentIndex + 1) -eq 0)
-            {
-                return ($haystackCurrentIndex + 1)
-            }
-
-            # Reset the needle index counter
-            $needleCurrentIndex = $needleLastIndex
-
-            # Move the haystack index back to the location of the match that caused us to enter this if block.
-            $haystackCurrentIndex = $startIndex
-        }
-
-        # Step to the next byte in the registry array
-        $haystackCurrentIndex -= 1
-    }
-    return -1
+	$newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+	$newProcess.Arguments = $myInvocation.MyCommand.Definition;
+	$newProcess.Verb = "runas";
+	[System.Diagnostics.Process]::Start($newProcess);
+	exit
 }
-
-# The header and trailer patterns seem to be the same for all modern apps, so as of right now it isn't really needed to
-# have separate hash table entries for each app. I'm doing it anyway to add some futureproofing/flexibility.
-$taskbarEntryHeaderBytes = @(0,51,6,0,0,20,0,31,128,155,212,52);
-$taskbarEntryHeaderBytesThatDontMatter = @(0,1,2,5,9,10);
-$taskbarEntryTrailerBytes = @(34,0,0,0,30,0,239,190,2,0,85,0,115,0,101,0,114,0,80,0,105,0,110,0,110,0,101,0,100,0,0,0,59,5,0,0);
-$taskbarEntryTrailerBytesThatDontMatter = @(32,33);
-
-$apps = @(
-    @{
-        "appName" = "Microsoft Edge"
-        "appThumbprint" = @(38,0,0,0,77,0,105,0,99,0,114,0,111,0,115,0,111,0,102,0,116,0,46,0,77,0,105,0,99,0,114,0,111,0,115,0,111,0,102,0,116,0,69,0,100,0,103,0,101);
-        "taskbarEntryHeaderBytes" = $taskbarEntryHeaderBytes
-        "taskbarEntryHeaderBytesThatDontMatter" = $taskbarEntryHeaderBytesThatDontMatter
-        "taskbarEntryTrailerBytes" = $taskbarEntryTrailerBytes
-        "taskbarEntryTrailerBytesThatDontMatter" = $taskbarEntryTrailerBytesThatDontMatter
-    }
-    @{
-        "appName" = "Microsoft Windows Store"
-        "appThumbprint" = @(37,0,0,0,77,0,105,0,99,0,114,0,111,0,115,0,111,0,102,0,116,0,46,0,87,0,105,0,110,0,100,0,111,0,119,0,115,0,83,0,116,0,111,0,114,0,101);
-        "taskbarEntryHeaderBytes" = $taskbarEntryHeaderBytes
-        "taskbarEntryHeaderBytesThatDontMatter" = $taskbarEntryHeaderBytesThatDontMatter
-        "taskbarEntryTrailerBytes" = $taskbarEntryTrailerBytes
-        "taskbarEntryTrailerBytesThatDontMatter" = $taskbarEntryTrailerBytesThatDontMatter
-    }
-)
-
-# Start with the assumption that no apps are pinned.
-$atLeastOneAppIsPinned = $false
-
-foreach ($app in $apps)
-{
-    # Get the current taskbar registry key
-    $haystack = (Get-ItemProperty -Path "hkcu:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Taskband" -Name "Favorites").Favorites
-
-    # First, we check for the app's thumbprint.
-    $needleStart = findTheNeedle $app["appThumbprint"] $haystack
-
-    # Carry on only if we found the thumbprint
-    if ($needleStart -ne -1)
-    {
-        # Look for the trailer pattern starting from the first byte after the thumbprint
-        $firstIndexAfterNeedle = $needleStart + $app["appThumbprint"].Length
-        $lastIndexOfEntry = (findTheNeedle $app["taskbarEntryTrailerBytes"] $haystack $firstIndexAfterNeedle $app["taskbarEntryTrailerBytesThatDontMatter"]) + ($app["taskbarEntryTrailerBytes"].Length - 1)
-
-        # Carry on only if we found the trailer pattern
-        if (($lastIndexOfEntry) -ne -1)
-        {
-            # Look for the header pattern going backwards from the first byte before the thumbprint
-            $firstIndexOfEntry = findTheNeedleReverse $app["taskbarEntryHeaderBytes"] $haystack $needleStart $app["taskbarEntryHeaderBytesThatDontMatter"]
-
-            # Carry on only if we found the header pattern
-            if (($firstIndexOfEntry) -ne -1)
-            {
-                $atLeastOneAppIsPinned = $true
-
-                # Create a new version of the taskbar registry entry without the app we're unpinning
-                if ($firstIndexOfEntry -eq 0) 
-                {
-                    $newArray = $haystack[$lastIndexOfEntry..($haystack.Length)]
-                }
-                else
-                {
-                    $newArray = $haystack[0..($firstIndexOfEntry - 1)] + $haystack[$lastIndexOfEntry..($haystack.Length)]
-                }
-                # Overwrite the old registry key with the new one
-                New-ItemProperty -Path "hkcu:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Taskband" -Name "Favorites" -Value $newArray -PropertyType Binary -Force | Out-Null
-            }
-        }
-    }
-}
-
+###############
 
 function Pin-App {    param(
         [string]$appname,
@@ -188,12 +41,8 @@ function Pin-App {    param(
 
 Get-StartApps | ForEach-Object { Pin-App $_.name -unpin }
 
-
-Invoke-Expression "taskkill /f /im explorer.exe"
-Invoke-Expression "start explorer.exe"
-
-
 Pin-App "Calculator" -pin
-Pin-App "This PC" -pin
-Pin-App "Settings" -pin
+Pin-App "Control Panel" -pin
+Pin-App "Photos" -pin
+Pin-App "Check For Updates" -pin
 Pin-App "Remote Desktop Connection" -pin
