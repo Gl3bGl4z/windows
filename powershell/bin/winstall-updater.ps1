110d9baa44170366d457e9148a20a7d0f581d067
Ad3t0 <ad3t000@gmail.com>
2018-10-20 02:34:52 -0600

    update

--- powershell/bin/winstall-updater.ps1
+++ powershell/bin/winstall-updater.ps1
@@ -49,12 +49,18 @@ else
 }
 ##############
 
+Write-Host "Searching for updates..."
+
 Copy-Item "$($dir)\PSWindowsUpdate" -Destination "$($env:USERPROFILE)\Documents\WindowsPowerShell\Modules\PSWindowsUpdate\" -Recurse -Force
 
 Import-Module PSWindowsUpdate
 
 Get-WUInstall -AcceptAll
 
+Clear
+
 Write-Host "Finished looking for updates."
 
 Get-WURebootStatus
+
+Read-Host "Press ENTER to exit" 
