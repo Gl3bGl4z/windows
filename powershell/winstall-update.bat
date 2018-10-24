30ea730405123aa7ac28f2bc1e0fd3a614a75612
Ad3t0 <ad3t000@gmail.com>
2018-10-23 21:34:05 -0600

    rename

---
+++ powershell/winstall-update.bat
@@ -0,0 +1,1 @@
+PowerShell.exe -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Ad3t0/windows/master/powershell-core/winstall-update.ps1'))"
