86fea02dd186befe66fbdd2fda3fe253fc8581e5
Ad3t0 <ad3t000@gmail.com>
2018-10-23 21:44:54 -0600

    updates

--- powershell-core/winstall-update.ps1
+++ powershell-core/winstall-update.ps1
@@ -22,46 +22,13 @@ else
 }
 ##############
 
-$scriptpath = $MyInvocation.MyCommand.Path
-$dir = Split-Path $scriptpath
-Set-Location $dir
 
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
-
-Copy-Item "$dir\PSWindowsUpdate" -Destination "$($env:USERPROFILE)\Documents\WindowsPowerShell\Modules\" -Recurse -Force
-
-$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
-$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
-$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
-if ($myWindowsPrincipal.IsInRole($adminRole))
-{
-	$Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
-	$Host.UI.RawUI.BackgroundColor = "DarkBlue"
-	clear-host
-}
-else
-{
-	$newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
-	$newProcess.Arguments = $myInvocation.MyCommand.Definition;
-	$newProcess.Verb = "runas";
-	[System.Diagnostics.Process]::Start($newProcess);
-	exit
-}
-##############
-
-Write-Host "Searching for updates..."
 
 Install-Module -Name PSWindowsUpdate -Force
 
+Clear-Host
 
-
-
-
-
-
-
-
-
+Write-Host "Searching for updates..."
 
 #Copy-Item "$($dir)\PSWindowsUpdate" -Destination "$($env:USERPROFILE)\Documents\WindowsPowerShell\Modules\PSWindowsUpdate\" -Recurse -Force
 
