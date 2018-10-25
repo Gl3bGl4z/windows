9be1f22ad539f04810963e4189e6d938027c2d64
Ad3t0 <ad3t000@gmail.com>
2018-10-24 20:03:20 -0600

    gist results

--- powershell-core/winstall-benchmark.ps1
+++ powershell-core/winstall-benchmark.ps1
@@ -23,14 +23,19 @@ Write-host "#       Windows 10 Benchmark Script     #"
 Write-host "#       Version: "$ver"	                #"
 Write-host "#########################################"
 Write-host
+New-Item -Path $env:TEMP -Name "winstall-core" -ItemType "directory" -Force >$null 2>&1
 Set-Location "$($env:TEMP)\winstall-core"
 while($confirmationupdate -ne "n" -and $confirmationupdate -ne "y")
 {	
 	$confirmationupdate = Read-Host "Run Windows 10 CPU benchmark? [y/n]"
 	Write-host
+}while($confirmationpost -ne "n" -and $confirmationpost -ne "y")
+{	
+	$confirmationpost = Read-Host "Post benchmark results to Gist? [y/n]"
+	Write-host
 }if($confirmationupdate -eq "y")
 {Write-Host "Running benchmark please wait..."
-Write-host
+	Write-host
 	if(!(Test-Path -Path "$($env:TEMP)\winstall-core\bench.exe" ))
 	{
 		[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -41,5 +46,13 @@ Write-host
 		#Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 	}
 	./bench multithread
+}if($confirmationpost -eq "y")
+{	if(!(Test-Path -Path "$($env:TEMP)\winstall-core\PsGist.psd1" ))
+	{	
+		(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/Ad3t0/windows/master/powershell-core/bin/PsGist.psd1') | out-file "$($env:TEMP)\winstall-core\PsGist.psd1" -force
+	}
+	(New-Object Net.WebClient).DownloadString('https://gist.githubusercontent.com/Ad3t0/7c36a22df7ff01cf50d138669281d9cf/raw') | out-file "$($env:TEMP)\winstall-core\results" -force
+		Import-Module .\PsGist.psd1
+	New-Gist -File "$($env:TEMP)\winstall-core\results" -Username Ad3t0
 }Read-Host "Press ENTER to exit"
 Exit
