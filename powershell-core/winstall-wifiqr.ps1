2e9ceaf31122d32a4e38f05d75fe834f24f82657
Ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2018-11-06 21:16:57 -0700

    update

--- powershell-core/winstall-wifiqr.ps1
+++ powershell-core/winstall-wifiqr.ps1
@@ -18,23 +18,27 @@ if ($myWindowsPrincipal.IsInRole($adminRole))
 	exit
 }##############
 $ver = "1.0.6"
-$data = netsh wlan show profile
-$datePattern = [Regex]::new("(?<=All User Profile     : ).*\S")
-$matches = $datePattern.Matches($data)
-$wifiprofile = $matches.Value
-$wifiprofile = $wifiprofile.Substring(0, $wifiprofile.IndexOf(' '))
-$data2 = netsh wlan show profile $wifiprofile key=clear
-$datePattern2 = [Regex]::new("(?<=Key Content            : ).*\S")
-$matches2 = $datePattern2.Matches($data2)
-$wifikey = $matches2.Value.split(' ')[0]
-$wifilink = "WIFI:S:$($wifiprofile);T:WPA;P:$($wifikey);;"
-$wifilink = [uri]::EscapeDataString($wifilink)
-$defaultbrowser = Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice" -Name "Progid"
-$URL = "https://chart.googleapis.com/chart?chs=547x547&cht=qr&chld=H|4&choe=UTF-8&chl=$($wifilink)"
-if($defaultbrowser.ProgID -like "Firefox*")
-{	
-	[System.Diagnostics.Process]::Start("firefox.exe","-private-window $URL")
-}else
-{	
-	[System.Diagnostics.Process]::Start("chrome.exe","--incognito $URL")
+try
+{	$data = netsh wlan show profile
+	$datePattern = [Regex]::new("(?<=All User Profile     : ).*\S")
+	$matches = $datePattern.Matches($data)
+	$wifiprofile = $matches.Value
+	$wifiprofile = $wifiprofile.Substring(0, $wifiprofile.IndexOf(' '))
+	$data2 = netsh wlan show profile $wifiprofile key=clear
+	$datePattern2 = [Regex]::new("(?<=Key Content            : ).*\S")
+	$matches2 = $datePattern2.Matches($data2)
+	$wifikey = $matches2.Value.split(' ')[0]
+	$wifilink = "WIFI:S:$($wifiprofile);T:WPA;P:$($wifikey);;"
+	$wifilink = [uri]::EscapeDataString($wifilink)
+	$defaultbrowser = Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice" -Name "Progid"
+	$URL = "https://chart.googleapis.com/chart?chs=547x547&cht=qr&chld=H|4&choe=UTF-8&chl=$($wifilink)"
+	if($defaultbrowser.ProgID -like "Firefox*")
+	{	
+		[System.Diagnostics.Process]::Start("firefox.exe","-private-window $URL")
+	}else
+	{	
+		[System.Diagnostics.Process]::Start("chrome.exe","--incognito $URL")
+	}
+}catch
+{	Write-Warning "Not connected to WIFI exiting..."
 }
