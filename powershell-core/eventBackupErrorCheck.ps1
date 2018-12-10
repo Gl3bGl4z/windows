9b6092d467e723be0df8b54b3bf2e7cbae8fe91e
Ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2018-12-10 11:54:07 -0700

    update

--- powershell-core/eventBackupErrorCheck.ps1
+++ powershell-core/eventBackupErrorCheck.ps1
@@ -153,4 +153,53 @@ if($inputOption -eq 2)
 
 
 
+####################################
+#Working
+###################################
+if(!(Test-Path -Path "$($env:ProgramData)\winstall-core\" ))
+{	New-Item -Path $env:ProgramData -Name "winstall-core" -ItemType "directory" -Force >$null 2>&1
+}
+
+	if($eventLogStatus)
+	{
+		$eventLogStatus
+		$eventLogStatusConfirmed = "True"
+		Write-Host "Event log Windows Backup abnormality detected" -foregroundcolor red
+		Write-Host
+	}
+	if($vssStatus -like "*Failed*" -like "*Stuck*" -like "*Unstable*" )
+	{
+		$vssStatus
+		$vssStatusConfirmed = "True"
+		Write-Host "Event log VSS Writer abnormality detected" -foregroundcolor red
+		Write-Host
+	}
+	if($vssStatusConfirmed -or $eventLogStatusConfirmed)
+	{
+		Write-Warning "Backup errors were detected"
+		Write-Host
+		Read-Host "Press ENTER to exit"
+	}
+	else
+	{
+		Write-Host "No Errors detected" -foregroundcolor green
+		Write-Host
+		Read-Host "Press ENTER to exit"
+	}
+
+
+if(!(Test-Path -Path "$($env:ProgramData)\winstall-core\empasshash" ))
+{
+    $pass = Read-Host "Enter Password"
+}
+
+$pass | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString | Out-File "$($env:ProgramData)\winstall-core\empasshash"
+
+$User = "ambientweatheralerts@gmail.com"
+
+$File = "$($env:ProgramData)\winstall-core\empasshash"
+
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, (Get-Content $File | ConvertTo-SecureString)
+
+Send-MailMessage -From $User -to "ambientweatheralerts@gmail.com" -Subject "subject" -Body "body" -SmtpServer "smtp.gmail.com" -port "587" -UseSsl -Credential $cred -DeliveryNotificationOption OnSuccess
 
