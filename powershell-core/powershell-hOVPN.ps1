1a251232acebdf83a6dc0c43a47136b991af0e3f
Ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2019-04-15 09:01:44 -0600

    changed download method

--- powershell-core/powershell-hOVPN.ps1
+++ powershell-core/powershell-hOVPN.ps1
@@ -16,38 +16,20 @@ if ($myWindowsPrincipal.IsInRole($adminRole))
 	[System.Diagnostics.Process]::Start($newProcess);
 	exit
 }##############
-$ver = "1.0.3"
+$ver = "1.0.4"
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco feature enable -n=allowGlobalConfirmation
 choco feature disable -n=checksumFiles
-choco install openvpn
-Remove-Item "C:\Users\Public\Desktop\OpenVPN GUI.lnk"
-Remove-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\TAP-Windows" -Recurse
-Remove-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\OpenVPN" -Recurse
+choco install openvpn megatools
+Remove-Item "C:\Users\Public\Desktop\OpenVPN GUI.lnk" >$null 2>&1
+Remove-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\TAP-Windows" -Recurse >$null 2>&1
+Remove-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\OpenVPN" -Recurse >$null 2>&1
 if(!(Test-Path -Path "$($env:TEMP)\winstall-core\" ))
 {New-Item -Path $env:TEMP -Name "winstall-core" -ItemType "directory" -Force >$null 2>&1
-}Set-Location "$($env:TEMP)\winstall-core"		
-$url = "https://github.com/Ad3t0/windows/raw/master/powershell-core/bin/7z.dll" 
-			$path = "$($env:TEMP)\winstall-core\7z.dll" 
-			if(!(Split-Path -parent $path) -or !(Test-Path -pathType Container (Split-Path -parent $path))) { 
-	$targetFile = Join-Path $pwd (Split-Path -leaf $path) 
-} 
-(New-Object Net.WebClient).DownloadFile($url, $path) 
-$path
-$url = "https://github.com/Ad3t0/windows/raw/master/powershell-core/bin/7z.exe" 
-$path = "$($env:TEMP)\winstall-core\7z.exe" 
-if(!(Split-Path -parent $path) -or !(Test-Path -pathType Container (Split-Path -parent $path))) { 
-	$targetFile = Join-Path $pwd (Split-Path -leaf $path) 
-} 
-(New-Object Net.WebClient).DownloadFile($url, $path) 
-$path
-$url = "https://github.com/Ad3t0/windows/raw/master/powershell-core/bin/config.zip" 
-$path = "$($env:TEMP)\winstall-core\config.zip" 
-if(!(Split-Path -parent $path) -or !(Test-Path -pathType Container (Split-Path -parent $path))) { 
-	$targetFile = Join-Path $pwd (Split-Path -leaf $path) 
-} 
-(New-Object Net.WebClient).DownloadFile($url, $path) 
-.\7z.exe x config.zip
-Move-Item -Path "$($env:TEMP)\winstall-core\client.ovpn" -Destination "C:\Program Files\OpenVPN\config\client.ovpn"
+}Set-Location "$($env:TEMP)\winstall-core"
+$user = Read-Host "Username"
+$pass = Read-Host "Password"
+megaget --path "C:\Program Files\OpenVPN\config" -u "$($user)" -p "$($pass)" "/Root/MEGAsync/VPN/Home/client.ovpn"
 cd "C:\Program Files\OpenVPN\bin\"
 .\openvpn-gui.exe
+Read-Host
