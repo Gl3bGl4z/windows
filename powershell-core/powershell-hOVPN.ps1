98f8230018e398d5a957ed434ec9130f1ef29ab0
Ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2019-05-08 08:44:32 -0600

    update

--- powershell-core/powershell-hOVPN.ps1
+++ powershell-core/powershell-hOVPN.ps1
@@ -15,7 +15,7 @@ if ($myWindowsPrincipal.IsInRole($adminRole))
 	$newProcess.Verb = "runas";
 	[System.Diagnostics.Process]::Start($newProcess);
 	exit
-} $ver = "1.2.7"
+} $ver = "1.2.8"
 $killProcess = Get-Process "openvpn-gui" -ErrorAction SilentlyContinue
 if ($killProcess) {
 	Stop-Process -Name "mstsc" > $null 2>&1
@@ -27,13 +27,13 @@ if ($killProcess) {
 	{ [Environment]::SetEnvironmentVariable("Path",[Environment]::GetEnvironmentVariable("Path",[EnvironmentVariableTarget]::Machine) + ";C:\ProgramData\powershell-bin",[EnvironmentVariableTarget]::Machine)
 	} New-Item -Path $env:ProgramData -Name "powershell-bin" -ItemType "directory" -Force > $null 2>&1
 	if (!(Test-Path -Path "C:\Program Files\OpenVPN\config\client.ovpn"))
-	{ Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+	{ $user = Read-Host "Username"
+		$pass = Read-Host "Password"
+		Clear-Host
+		Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 		choco feature enable -n=allowGlobalConfirmation
 		choco feature disable -n=checksumFiles
 		choco install openvpn megatools
-		$user = Read-Host "Username"
-		$pass = Read-Host "Password"
-		Clear-Host
 		megaget --path "C:\Program Files\OpenVPN\config" -u $user -p $pass "/Root/MEGAsync/VPN/Home/client.ovpn"
 	} Remove-Item "C:\Users\Public\Desktop\OpenVPN GUI.lnk" > $null 2>&1
 	Remove-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\OpenVPN" -Recurse > $null 2>&1
