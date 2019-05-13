ad7f46fcac3dbb6828405373096b6dd455588253
Ad3t0 <31329475+Ad3t0@users.noreply.github.com>
2019-05-13 13:45:42 -0600

    update

--- powershell-core/powershell-hOVPN.ps1
+++ powershell-core/powershell-hOVPN.ps1
@@ -15,7 +15,7 @@ if ($myWindowsPrincipal.IsInRole($adminRole))
 	$newProcess.Verb = "runas";
 	[System.Diagnostics.Process]::Start($newProcess);
 	exit
-} $ver = "1.3.6"
+} $ver = "1.3.7"
 $text1 = @'
      _       _ _____ _    ___
     / \   __| |___ /| |_ / _ \
@@ -32,16 +32,15 @@ Write-Host $ver -ForegroundColor Green
 $killProcess = Get-Process "openvpn-gui" -ErrorAction SilentlyContinue
 if ($killProcess) {
 	Stop-Process -Name "mstsc" > $null 2>&1
-	Write-Host "Stopping VPN Processes..." -ForegroundColor Yellow
-	. 'C:\Program Files\OpenVPN\bin\openvpn-gui.exe' --command disconnect_all
+	. '$($env:ProgramFiles)\OpenVPN\bin\openvpn-gui.exe' --command disconnect_all
 	Start-Sleep -s 20
-	. 'C:\Program Files\OpenVPN\bin\openvpn-gui.exe' --command exit
+	. '$($env:ProgramFiles)\OpenVPN\bin\openvpn-gui.exe' --command exit
 } else
-{ if ($env:Path -notlike "*;C:\ProgramData\powershell-bin*")
+{ if ($env:Path -notlike "*;$($env:ProgramData)\powershell-bin*")
 	{ Write-Host "Starting VPN Processes..." -ForegroundColor Yellow
-		[Environment]::SetEnvironmentVariable("Path",[Environment]::GetEnvironmentVariable("Path",[EnvironmentVariableTarget]::Machine) + ";C:\ProgramData\powershell-bin",[EnvironmentVariableTarget]::Machine)
+		[Environment]::SetEnvironmentVariable("Path",[Environment]::GetEnvironmentVariable("Path",[EnvironmentVariableTarget]::Machine) + ";$($env:ProgramData)\powershell-bin",[EnvironmentVariableTarget]::Machine)
 	} New-Item -Path $env:ProgramData -Name "powershell-bin" -ItemType "directory" -Force > $null 2>&1
-	if (!(Test-Path -Path "C:\Program Files\OpenVPN\config\client.ovpn"))
+	if (!(Test-Path -Path "$($env:ProgramFiles)\OpenVPN\config\client.ovpn"))
 	{ $user = Read-Host "Username"
 		$pass = Read-Host "Password"
 		Clear-Host
@@ -53,29 +52,29 @@ if ($killProcess) {
 		choco feature enable -n=allowGlobalConfirmation
 		choco feature disable -n=checksumFiles
 		choco install openvpn megatools
-		megaget --path "C:\Program Files\OpenVPN\config" -u $user -p $pass "/Root/MEGAsync/VPN/Home/client.ovpn"
+		megaget --path "$($env:ProgramFiles)\OpenVPN\config" -u $user -p $pass "/Root/MEGAsync/VPN/Home/client.ovpn"
 	}
-	if (!(Test-Path -Path "C:\Program Files\AutoHotkey\AutoHotkey.exe"))
+	if (!(Test-Path -Path "$($env:ProgramFiles)\AutoHotkey\AutoHotkey.exe"))
 	{
 		choco install autohotkey
 	}
 	Remove-Item "$($env:USERPROFILE)\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\hkeys.ahk" > $null 2>&1
-	Remove-Item "C:\Users\Public\Desktop\OpenVPN GUI.lnk" > $null 2>&1
-	Remove-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\OpenVPN" -Recurse > $null 2>&1
-	Remove-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\TAP-Windows" -Recurse > $null 2>&1
-	Remove-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Gpg4win" -Recurse > $null 2>&1
+	Remove-Item "$($env:Public)\Desktop\OpenVPN GUI.lnk" > $null 2>&1
+	Remove-Item "$($env:ProgramData)\Microsoft\Windows\Start Menu\Programs\OpenVPN" -Recurse > $null 2>&1
+	Remove-Item "$($env:ProgramData)\Microsoft\Windows\Start Menu\Programs\TAP-Windows" -Recurse > $null 2>&1
+	Remove-Item "$($env:ProgramData)\Microsoft\Windows\Start Menu\Programs\Gpg4win" -Recurse > $null 2>&1
 	Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "OPENVPN-GUI" > $null 2>&1
 	Remove-Item "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\OpenVPN" -Recurse > $null 2>&1
 	Remove-Item "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\TAP-Windows" -Recurse > $null 2>&1
 	Remove-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\GPG4Win" -Recurse > $null 2>&1
 	Remove-Item "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\GPG4Win" -Recurse > $null 2>&1
-	(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/Ad3t0/windows/master/powershell-core/bin/vpn.bat') | Out-File "C:\ProgramData\powershell-bin\vpn.bat" -Force -Encoding default
-	(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/Ad3t0/windows/master/powershell-core/bin/hkeys.ahk') | Out-File "C:\ProgramData\powershell-bin\hkeys.ahk" -Force -Encoding default
-	schtasks /Create /SC ONLOGON /TN hkeys /TR "C:\ProgramData\powershell-bin\hkeys.ahk" /RL HIGHEST /F
+	(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/Ad3t0/windows/master/powershell-core/bin/vpn.bat') | Out-File "$($env:ProgramData)\powershell-bin\vpn.bat" -Force -Encoding default
+	(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/Ad3t0/windows/master/powershell-core/bin/hkeys.ahk') | Out-File "$($env:ProgramData)\powershell-bin\hkeys.ahk" -Force -Encoding default
+	schtasks /Create /SC ONLOGON /TN hkeys /TR "$($env:ProgramData)\powershell-bin\hkeys.ahk" /RL HIGHEST /F
 	$ahkProcess = Get-Process "AutoHotkey" -ErrorAction SilentlyContinue
 	if (!$ahkProcess) {
-		. "C:\ProgramData\powershell-bin\hkeys.ahk"
+		. "$($env:ProgramData)\powershell-bin\hkeys.ahk"
 	}
-	. 'C:\Program Files\OpenVPN\bin\openvpn-gui.exe' --connect client.ovpn
+	. '$($env:ProgramFiles)\OpenVPN\bin\openvpn-gui.exe' --connect client.ovpn
 	. 'C:\Windows\System32\mstsc.exe' /multimon
 }
